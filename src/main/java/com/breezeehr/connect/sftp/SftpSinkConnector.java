847fff9bd8ff3770a4eb9fdbd4ea15966de0b56c
Francis Avila <favila@breezeehr.com>
2017-03-21 17:11:40 -0500

    Add more configuration options; accept string or byte file contents.
    * PRIVATE_KEY and PASSWORD options now work.
    * Added FILE_PREFIX option.
    * taskConfigs() always returns maxTasks number of task configs instead of just one.
    * Update gitignore for IntelliJ
    * file contents can now be bytes, or string (will be encoded as UTF-8)
    * Cleanup sessions and connections more safely on stop()
    * silence some maven build warnings with pom settings.

--- src/main/java/com/breezeehr/connect/sftp/SftpSinkConnector.java
+++ src/main/java/com/breezeehr/connect/sftp/SftpSinkConnector.java
@@ -5,27 +5,27 @@ import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.sink.SinkConnector;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.apache.kafka.common.config.ConfigDef.*;
 
-/**
- * Created by favila on 3/20/17.
- */
 public class SftpSinkConnector extends SinkConnector {
-    public static final String USERNAME = "sftp.username";
-    public static final String HOSTNAME = "sftp.hostname";
-    public static final String PORT = "sftp.port";
-    public static final String PASSWORD = "sftp.password";
-    public static final String PRIVATE_KEY = "sftp.private-key";
+    static final String USERNAME = "sftp.username";
+    static final String HOSTNAME = "sftp.hostname";
+    static final String PORT = "sftp.port";
+    static final String PASSWORD = "sftp.password";
+    static final String PRIVATE_KEY = "sftp.private-key";
+    static final String FILE_PREFIX = "sftp.file-prefix";
     private static final ConfigDef CONFIG_DEF = new ConfigDef()
             .define(USERNAME, Type.STRING, Importance.HIGH, "Username used to connect to the sftp.hostname")
             .define(HOSTNAME, Type.STRING, Importance.HIGH, "The sftp host to connect to")
             .define(PORT, Type.INT, 22, Importance.LOW, "The port number to connect to")
-            .define(PASSWORD, Type.PASSWORD, Importance.HIGH, "Password used to authenticate with sftp.hostname")
-            .define(PRIVATE_KEY, Type.STRING, NO_DEFAULT_VALUE, Importance.HIGH, "Base64-encoded private key used to authenticate with sftp.hostname",
-                    null, -1, Width.LONG, "Base64-encoded private key used to authenticate with sftp.hostname");
+            .define(PASSWORD, Type.PASSWORD, null, Importance.HIGH, "Password used to authenticate with sftp.hostname")
+            .define(PRIVATE_KEY, Type.STRING, null, Importance.HIGH, "Base64-encoded private key used to authenticate with sftp.hostname",
+                    null, -1, Width.LONG, "Base64-encoded private key used to authenticate with sftp.hostname")
+            .define(FILE_PREFIX, Type.STRING, "", Importance.MEDIUM, "String prepended to filenames before PUTing to remote sftp server. Allows you to, e.g., put all files in a particular directory.");
     private Map<String, String> config;
 
     public String version() {
@@ -33,7 +33,11 @@ public class SftpSinkConnector extends SinkConnector {
     }
 
     public void start(Map<String, String> map) {
-        config = map;
+        final String[] setting_keys = {USERNAME, HOSTNAME, PORT, PASSWORD, PRIVATE_KEY, FILE_PREFIX};
+        config = new HashMap<>(setting_keys.length);
+        for (String k : setting_keys) {
+            config.put(k, map.get(k));
+        }
     }
 
     public Class<? extends Task> taskClass() {
@@ -41,13 +45,15 @@ public class SftpSinkConnector extends SinkConnector {
     }
 
     public List<Map<String, String>> taskConfigs(int maxTasks) {
-        final ArrayList<Map<String, String>> cfgs = new ArrayList<>(1);
-        cfgs.add(config);
+        final ArrayList<Map<String, String>> cfgs = new ArrayList<>(maxTasks);
+        for (int i = 0; i < maxTasks; i++) {
+            cfgs.add(new HashMap<>(config));
+        }
         return cfgs;
     }
 
     public void stop() {
-
+        config = null;
     }
 
     public ConfigDef config() {
--- src/main/java/com/breezeehr/connect/sftp/SftpSinkTask.java
+++ src/main/java/com/breezeehr/connect/sftp/SftpSinkTask.java
@@ -12,12 +12,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Map;
 
-/**
- * Created by favila on 3/20/17.
- */
 public class SftpSinkTask extends SinkTask {
     private Session session;
     private ChannelSftp channel;
+    private String file_prefix;
 
     public String version() {
         return getClass().getPackage().getImplementationVersion();
@@ -25,6 +23,16 @@ public class SftpSinkTask extends SinkTask {
 
     public void start(Map<String, String> props) {
         final JSch ssh = new JSch();
+        final String private_key = props.get(SftpSinkConnector.PRIVATE_KEY);
+        if (private_key != null) {
+            try {
+                ssh.addIdentity("sftp-connector-identity",
+                        private_key.getBytes(StandardCharsets.UTF_8),
+                        null, null);
+            } catch (JSchException e) {
+                throw new ConnectException("Could not add sftp.private-key", e);
+            }
+        }
         try {
             session = ssh.getSession(props.get(SftpSinkConnector.USERNAME),
                     props.get(SftpSinkConnector.HOSTNAME),
@@ -32,6 +40,8 @@ public class SftpSinkTask extends SinkTask {
         } catch (JSchException e) {
             throw new ConnectException("Could not create ssh session", e);
         }
+        if (props.get(SftpSinkConnector.PASSWORD) != null)
+            session.setPassword(props.get(SftpSinkConnector.PASSWORD));
         try {
             session.connect(5000);
         } catch (JSchException e) {
@@ -44,24 +54,46 @@ public class SftpSinkTask extends SinkTask {
             session.disconnect();
             throw new ConnectException("Could not open sftp channel", e);
         }
+        file_prefix = props.get(SftpSinkConnector.FILE_PREFIX);
     }
 
     public void put(Collection<SinkRecord> records) {
         for (SinkRecord sr : records) {
-            final InputStream src = new ByteArrayInputStream(((String) sr.value())
-                    .getBytes(StandardCharsets.UTF_8));
+            final Object key = sr.key();
+            final Object value = sr.value();
+            final String filename;
+            if (key instanceof String) {
+                filename = file_prefix + sr.key();
+            } else {
+                throw new ConnectException("SFTP connector requires key (the desired filename) be a string");
+            }
+            final InputStream file_bytes;
+            if (value instanceof byte[]) {
+                file_bytes = new ByteArrayInputStream((byte[]) value);
+            } else if (value instanceof CharSequence) {
+                file_bytes = new ByteArrayInputStream(value.toString().getBytes(StandardCharsets.UTF_8));
+            } else {
+                throw new ConnectException("SFTP connector requires value (the desired file contents) be a string or bytes");
+            }
             try {
-                channel.put(src, (String) sr.key());
+                channel.put(file_bytes, filename);
             } catch (SftpException e) {
-                throw new RetriableException("Could not upload file " + (String) sr.key(), e);
+                throw new RetriableException("Could not upload file " + filename, e);
             }
         }
     }
 
     public void stop() {
-        channel.disconnect();
-        session.disconnect();
-        channel = null;
-        session = null;
+        file_prefix = null;
+        try {
+            channel.disconnect();
+        } finally {
+            channel = null;
+            try {
+                session.disconnect();
+            } finally {
+                session = null;
+            }
+        }
     }
 }
